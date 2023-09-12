6ef60373284360599e36035bd2efcb0b5ab14d3f
RivinHD <58261670+RivinHD@users.noreply.github.com>
2023-09-12 22:22:09 +0200

    Add project files.

---
+++ DeskBandTest/DeskBand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSDeskBand;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DeskBandTest
+{
+
+    [ComVisible(true)]
+    [Guid("90C19864-6366-459F-BA63-67B2F9C90E77")]
+    [CSDeskBandRegistration(Name = "DeskBandTest", ShowDeskBand = true)]
+    public partial class Deskband : CSDeskBandWin
+    {
+        Control _control;
+        public Deskband()
+        {
+            Options.MinHorizontalSize = new Size(100, 30);
+            _control = new UserControl1();
+        }
+
+        protected override Control Control => _control; // Returns your main control
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+1. Build the Solution. This should build a .dll
+1. Open `Developer Command Prompt for VS 2022` with priviliges.
+ the `Developer Command 1. Go to the bin/Debug directory insidePrompt for VS 2022`
+1. execute `c:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /codebase DeskBandTest.dll`
